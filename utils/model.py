989af2541d9e06a395d66cbcc4c07dacceda641c
darkcom109 <garciavalentino2006@gmail.com>
2025-10-01 18:45:35 +0100

    Users can now see their code

--- utils/model.py
+++ utils/model.py
@@ -13,9 +13,12 @@ def generate(user_prompt: str):
         input=f"""
             Generate a modern, Tailwind-styled HTML page based on this request: {user_prompt}.
             Use google fonts
-            Do NOT include <!DOCTYPE>, <html>, or <body> tags.
+            Do NOT include <!DOCTYPE>, <html>, or <body> tags, and do not include '''html and ''' at the end AS IF YOUR LIFE DEPENDED ON IT.
             Output only HTML code for the body content.
             """,
-        max_output_tokens=3000,
+        max_output_tokens=9999,
+        reasoning={"effort": "high"}
     )
+    if response.output_text[0:7] == "```html":
+        return response.output_text[7:len(response.output_text) - 3]
     return response.output_text
